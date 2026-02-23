d5aa0a1f16e55ebb66ce38e118db35e8ad54ea3a
benn-christensen <bnch@eaaa.dk>
2026-02-23 09:28:11 +0100

    Add project files.

---
+++ examples/Client/Client.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Client
+{
+    internal class Client
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = await factory.CreateConnectionAsync();
+
+            using var channel = await connection.CreateChannelAsync();
+
+            var replyQueue = await channel.QueueDeclareAsync();
+            await channel.QueueDeclareAsync("request-queue", exclusive: false);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            consumer.ReceivedAsync += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Reply Recieved: {message}");
+                return Task.CompletedTask;
+            };
+
+            await channel.BasicConsumeAsync(queue: replyQueue.QueueName, autoAck: true, consumer: consumer);
+
+            var properties = new BasicProperties()
+            {
+                ReplyTo = replyQueue.QueueName,
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            var message = "Can I request a reply?";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            Console.WriteLine($"Sending Request: {properties.CorrelationId}");
+
+            await channel.BasicPublishAsync(string.Empty, "request-queue", true, properties, body);
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
---
+++ examples/Server/Server.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Server
+{
+    internal class Server
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = await factory.CreateConnectionAsync();
+
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync("request-queue", exclusive: false);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                Console.WriteLine($"Received Request: {ea.BasicProperties.CorrelationId}");
+
+                var replyMessage = $"This is your reply: {ea.BasicProperties.CorrelationId}";
+                var replyProperties = new BasicProperties
+                {
+                    CorrelationId = ea.BasicProperties.CorrelationId
+                };
+                var body = Encoding.UTF8.GetBytes(replyMessage);
+
+                if (string.IsNullOrEmpty(ea.BasicProperties.ReplyTo))
+                {
+                    Console.WriteLine("No reply-to property set. Cannot send reply.");
+                    return;
+                }
+
+                await channel.BasicPublishAsync(
+                    exchange: string.Empty,
+                    routingKey: ea.BasicProperties.ReplyTo,
+                    mandatory: true,
+                    basicProperties: replyProperties,
+                    body: body
+                );
+            };
+
+            await channel.BasicConsumeAsync(queue: "request-queue", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Waiting for request");
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
